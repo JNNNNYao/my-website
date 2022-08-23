0b2c7231c9856d85af4c0bcaae47ca67ec537224
姚潔恩 <matt1129yao@gmail.com>
2022-08-23 16:28:36 +0800

    [UPDATE] responsive navbar

--- src/pages/blocks/Navigation.js
+++ src/pages/blocks/Navigation.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import '../../css/Navigation.scss';
 
 function Navigation() {
+    const [expanded, setExpanded] = useState(false)
+
     return (
         <header className="header">
             <h1 className="index-link">
@@ -18,6 +22,23 @@ function Navigation() {
                     </li>
                 </ul>
             </nav>
+            <nav className="navbar">
+                <button onClick={() => {
+                    setExpanded(!expanded)
+                }}>
+                    <FontAwesomeIcon icon={faBars} size="2x" beat />
+                </button>
+            </nav>
+            <div className={expanded? "menu": "menu-closed"}>
+                <ul>
+                    <li key="About">
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li key="Projects">
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 }
