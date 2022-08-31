c41d02f8bc2a0e6c798d6586eb4ff8e03d45f36e
姚潔恩 <matt1129yao@gmail.com>
2022-08-31 13:54:15 +0800

    [FIX] icon size

--- src/pages/blocks/Icons.js
+++ src/pages/blocks/Icons.js
@@ -7,9 +7,9 @@ import '../../css/Icons.scss';
 function Icons() {
     return (
         <div className="icons">
-            <a href="https://github.com/JNNNNYao/"><button><FontAwesomeIcon icon={faGithub}/> Github</button></a>
-            <a href="https://www.linkedin.com/in/MatthewYaoJN"><button><FontAwesomeIcon icon={faLinkedinIn}/> LinkedIn</button></a>
-            <a href="mailto:matt1129yao@gmail.com"><button><FontAwesomeIcon icon={faEnvelope}/> Email</button></a>
+            <a href="https://github.com/JNNNNYao/"><button><FontAwesomeIcon icon={faGithub} size='xs'/> Github</button></a>
+            <a href="https://www.linkedin.com/in/MatthewYaoJN"><button><FontAwesomeIcon icon={faLinkedinIn} size='xs'/> LinkedIn</button></a>
+            <a href="mailto:matt1129yao@gmail.com"><button><FontAwesomeIcon icon={faEnvelope} size='xs'/> Email</button></a>
         </div>
     );
 }
