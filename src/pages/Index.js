d895a248a66b1026e33f5a6a44d81c99b3515371
姚潔恩 <matt1129yao@gmail.com>
2022-12-04 15:01:29 +0800

    update information

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -22,7 +22,7 @@ function Index() {
                 {/* <h2>About</h2> */}
                 <p>
                 Hi, this is Matthew! Welcome to my website.
-                I am a research intern at <a href='https://www.mediatek.com/'>MediaTek</a>, mentored by Yi-Chen Lo, Roy Tseng, and Chia-Che Chang. My research interests lie in the fields of learning with limited data, low-level vision, and implicit neural representation.
+                I am a research intern at <a href='https://www.mediatek.com/'>MediaTek</a>, mentored by <a href='https://scholar.google.com.tw/citations?user=EPYQ48sAAAAJ&hl=zh-TW'>Yi-Chen Lo</a>, <a href='https://github.com/roytseng-tw'>Roy Tseng</a>, and <a href='https://scholar.google.com.tw/citations?user=FK1RcpoAAAAJ&hl=zh-TW'>Chia-Che Chang</a>. My research interests lie in the fields of learning with limited data, low-level vision, and implicit neural representation.
                 I received a B.S. degree in Computer Science from <a href='https://nthu-en.site.nthu.edu.tw/'>National Tsing Hua University</a>, advised by <a href='http://elsalab.ai/about'>Prof. Chun-Yi Lee</a>.
                 Currently, I am applying for Fall 2023 graduate programs in the U.S.
                 </p>
