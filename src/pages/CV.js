ec191d4577ab3b473478b3193d41520d928576ed
姚潔恩 <matt1129yao@gmail.com>
2023-03-02 14:44:24 +0800

    [UPDATE] CV logo

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -3,6 +3,8 @@ import Navigation from './blocks/Navigation';
 import Footer from './blocks/Footer'
 import '../css/CV.scss'
 
+const { PUBLIC_URL } = process.env;
+
 function CV() {
   return (
     <div class="body">
@@ -11,102 +13,116 @@ function CV() {
         <div className="wrapper">
           <section className="intro">
             <header>
-              <h2>CV</h2>
+              <h2>Curriculum Vitae</h2>
             </header>
           </section>
           <section className="resume">
             <header>
               <h2>Education</h2>
             </header>
-            <div className="item">
-              <div className="title_and_date">
-                <h3>
-                  <a href="https://nthu-en.site.nthu.edu.tw/">National Tsing Hua University</a>
-                </h3>
-                <div>
-                  <i>Sep. 2018 – June 2022</i>
+            <div className="image_and_item">
+              <img src={`${PUBLIC_URL}/NTHU.jpeg`} alt="" />
+              <div className="item">
+                <div className="title_and_date">
+                  <h3>
+                    <a href="https://nthu-en.site.nthu.edu.tw/">National Tsing Hua University</a>
+                  </h3>
+                  <div>
+                    <i>Sep. 2018 – June 2022</i>
+                  </div>
                 </div>
+                <i>Bachelor of Science in Computer Science</i>
+                <ul>
+                  <li>GPA: 4.27 / 4.3</li>
+                  <li>Ranking: 1 / 142</li>
+                  <li>4 Academic Achievement Awards</li>
+                </ul>
               </div>
-              <i>Bachelor of Science in Computer Science</i>
-              <ul>
-                <li>Overall GPA: 4.27 / 4.3</li>
-                <li>Major GPA: 4.29 / 4.3</li>
-                <li>Ranking: 1 / 142</li>
-                <li>Academic Achievement Award (Spring 2019, Spring 2020, Fall 2020, Spring 2021)</li>
-              </ul>
             </div>
           </section>
           <section className="resume">
             <header>
               <h2>Research Experience</h2>
             </header>
-            <div className="item">
-              <div className="title_and_date">
-                <h3>
-                  <a href="https://www.mediatek.com/">MediaTek</a>
-                </h3>
-                <div>
-                  <i>Jan. 2022 - Dec. 2022</i>
+            <div className="image_and_item">
+              <img src={`${PUBLIC_URL}/mediatek.png`} alt="" />
+              <div className="item">
+                <div className="title_and_date">
+                  <h3>
+                    <a href="https://www.mediatek.com/">MediaTek</a>
+                  </h3>
+                  <div>
+                    <i>Jan. 2022 - Dec. 2022</i>
+                  </div>
                 </div>
+                <i>Research Intern in Intelligence Vision Processing team</i>
+                <ul>
+                  <li>Research topic: Super-Resolution.</li>
+                </ul>
               </div>
-              <i>Research Intern in Intelligence Vision Processing team</i>
-              <ul>
-                <li>Research topic: Super-Resolution.</li>
-              </ul>
             </div>
-            <div className="item">
-              <div className="title_and_date">
-                <h3>
-                  <a href="http://elsalab.ai/about">ELSA Lab</a>
-                </h3>
-                <div>
-                  <i>Feb. 2021 - June 2022</i>
+            <div className="image_and_item">
+              <img src={`${PUBLIC_URL}/elsalab.png`} alt="" />
+              <div className="item">
+                <div className="title_and_date">
+                  <h3>
+                    <a href="http://elsalab.ai/about">ELSA Lab</a>
+                  </h3>
+                  <div>
+                    <i>Feb. 2021 - June 2022</i>
+                  </div>
                 </div>
+                <i>Undergraduate Research Assistant at National Tsing Hua University</i>
+                <ul>
+                  <li>Research topic: Unsupervised Domain Adaptation & Model-Based Reinforcement Learning.</li>
+                </ul>
               </div>
-              <i>Undergraduate Research Assistant at National Tsing Hua University</i>
-              <ul>
-                <li>Research topic: Unsupervised Domain Adaptation & Model-Based Reinforcement Learning.</li>
-              </ul>
             </div>
           </section>
           <section className="resume">
             <header>
               <h2>Work Experience</h2>
             </header>
-            <div className="item">
-              <div className="title_and_date">
-                <h3>
-                  <a href="https://www.itri.org.tw/english/">Industrial Technology Research Institute</a>
-                </h3>
-                <div>
-                  <i>July 2020 – Sep. 2020</i>
+            <div className="image_and_item">
+              <img src={`${PUBLIC_URL}/ITRI.jpeg`} alt="" />
+              <div className="item">
+                <div className="title_and_date">
+                  <h3>
+                    <a href="https://www.itri.org.tw/english/">Industrial Technology Research Institute</a>
+                  </h3>
+                  <div>
+                    <i>July 2020 – Sep. 2020</i>
+                  </div>
                 </div>
+                <i>Data Science Intern at <a href="https://www.itri.org.tw/english/Computational-Intelligence-Technology-Center?CRWP=617753153253043007">Computational Intelligence Technology Center</a></i>
+                <ul>
+                  <li>Implement a data pipeline to preprocess and analyze data from an optoelectronic manufacturing factory.</li>
+                  <li>Perform feature engineering and build Machine Learning models to predict the factory's production quality.</li>
+                </ul>
               </div>
-              <i>Data Science Intern at <a href="https://www.itri.org.tw/english/Computational-Intelligence-Technology-Center?CRWP=617753153253043007">Computational Intelligence Technology Center</a></i>
-              <ul>
-                <li>Implement a data pipeline to preprocess and analyze data from an optoelectronic manufacturing factory.</li>
-                <li>Perform feature engineering and build Machine Learning models to predict the factory's production quality.</li>
-              </ul>
             </div>
           </section>
           <section className="resume">
             <header>
               <h2>Teaching Experience</h2>
             </header>
-            <div className="item">
-              <div className="title_and_date">
-                <h3>
-                  <a href="https://nthu-en.site.nthu.edu.tw/">National Tsing Hua University</a>
-                </h3>
-                <div>
-                  <i>Spring 2021</i>
+            <div className="image_and_item">
+              <img src={`${PUBLIC_URL}/NTHU.jpeg`} alt="" />
+              <div className="item">
+                <div className="title_and_date">
+                  <h3>
+                    <a href="https://nthu-en.site.nthu.edu.tw/">National Tsing Hua University</a>
+                  </h3>
+                  <div>
+                    <i>Spring 2021</i>
+                  </div>
                 </div>
+                <i>Teaching Assistant of Introduction to Programming</i>
+                <ul>
+                  <li>Help the professor organize the curriculum and guide students in developing their coding ability in C++.</li>
+                  <li>Offer tutorial in English.</li>
+                </ul>
               </div>
-              <i>Teaching Assistant of Introduction to Programming</i>
-              <ul>
-                <li>Help the professor organize the curriculum and guide students in developing their coding ability in C++.</li>
-                <li>Use English when tutoring because the course is offered in English.</li>
-              </ul>
             </div>
           </section>
           <section className="resume">
